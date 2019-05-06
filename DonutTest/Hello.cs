6c3c1d8c803a9f4838f41521a48d0ce1a1e5f7d6
odzhan <odzhan@protonmail.com>
2019-05-06 03:47:40 +0100

    upd

---
+++ DonutTest/Hello.cs
@@ -0,0 +1,12 @@
+// A Hello World! program in C#.
+using System;
+namespace HelloWorld
+{
+    class Hello 
+    {
+        static void Main() 
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
