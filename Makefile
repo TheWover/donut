19bcdf018c7338e3966ea64fda6b424ac91012a9
odzhan <odzhan@protonmail.com>
2019-12-15 10:14:11 +0000

    upd

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ donut:
 	gcc -Wunused-function -Wall -fpack-struct=8 -DDONUT_EXE -I include donut.c hash.c encrypt.c format.c loader/clib.c -odonut lib/aplib64.a
 	gcc -Wunused-function -Wall -c -fpack-struct=8 -fPIC -I include donut.c hash.c encrypt.c format.c loader/clib.c 
 	ar rcs lib/libdonut.a donut.o hash.o encrypt.o format.o clib.o lib/aplib64.a
-	gcc -Wall -shared -o lib/libdonut.so donut.o hash.o encrypt.o format.o clib.o
+	gcc -Wall -shared -o lib/libdonut.so donut.o hash.o encrypt.o format.o clib.o lib/aplib64.a
 debug:
 	gcc -Wunused-function -ggdb -Wall -Wno-format -fpack-struct=8 -DDEBUG -DDONUT_EXE -I include donut.c hash.c encrypt.c format.c loader/clib.c lib/aplib64.a -odonut
 hash:
--- setup.py
+++ setup.py
@@ -3,6 +3,19 @@ from setuptools import Extension, setup, sys
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+static_libraries = ['aplib64']
+static_lib_dir = 'lib'
+libraries = []
+library_dirs = ['lib']
+
+if sys.platform == 'win64':
+    libraries.extend(static_libraries)
+    library_dirs.append(static_lib_dir)
+    extra_objects = []
+else: # POSIX
+    extra_objects = ['{}/lib{}.a'.format(static_lib_dir, l) for l in static_libraries]
+
+
 module = Extension(
         "donut",
         include_dirs=['include'],
@@ -14,7 +27,10 @@ module = Extension(
             'loader/clib.c',
             'donutmodule.c'
         ],
+        libraries=['lib/aplib64.a'],
+        extra_compile_args=['-static'],
         extra_link_args=['-static', 'lib/aplib64.a'],
+        extra_objects=extra_objects,
 )
 
 setup(
