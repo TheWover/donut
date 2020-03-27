2027c2d178fddbad0d7ba9291e749339fe1a3e0a
odzhan <odzhan@protonmail.com>
2020-03-27 04:27:41 +0000

    update structures

--- include/donut.h
+++ include/donut.h
@@ -390,6 +390,7 @@ typedef struct _DONUT_INSTANCE {
     
     int         type;                       // DONUT_INSTANCE_EMBED, DONUT_INSTANCE_HTTP 
     char        server[DONUT_MAX_NAME];     // staging server hosting donut module
+    char        auth[DONUT_MAX_NAME];       // username and password for web server
     char        http_req[8];                // just a buffer for "GET"
 
     uint8_t     sig[DONUT_MAX_NAME];        // string to hash
